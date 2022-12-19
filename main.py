7a6529c61ab01d629f766a9756035ef971f91846
despair <grobamist@gmail.com>
2022-12-20 01:49:18 +0400

    adding domens

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import os
 import uvicorn
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -44,6 +45,35 @@ def offProxy():
 	os.popen("sed -i '1s/http_access deny all/# http_access deny all/' /etc/squid/squid.conf").read()
 	os.popen("systemctl restart squid").read()
 	return {"status": 1, "message":"Proxy is off!"}
+
+@app.get("/domens")
+def getDomens():
+	list_of_apache_domens = os.listdir("./apache")
+	list_of_nginx_domens = os.listdir("./nginx")
+	list_of_domens = []
+	for apache_domen in list_of_apache_domens:
+		list_of_domens.append({
+			"name": apache_domen,
+			"server": "apache"
+		})
+	for nginx_domen in list_of_nginx_domens:
+		list_of_domens.append({
+			"name": nginx_domen,
+			"server": "nginx"
+		})
+	return {"domens": list_of_domens}
+
+class DomenModel(BaseModel):
+	name: str
+	server: str
+
+@app.post("/domens")
+def getDomens(domen_model: DomenModel):
+	cwd = os.getcwd()
+	target_dir = cwd +'/' + domen_model.server + '/' + domen_model.name
+	os.mkdir(target_dir)
+	with open(target_dir +'/index.html', 'w') as fp:
+		fp.write(domen_model.name + ' by ' + domen_model.server)
 
 
 if __name__ == "__main__":
